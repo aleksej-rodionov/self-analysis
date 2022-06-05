147204f6e2c9c63b60ee0f5b3715c69eb7fdbb9b
Aleksej <rodionovjr91@gmail.com>
2022-06-05 15:20:10 +0300

    Composables
    далее: перенести версии из buildscript в build.gradle :app

---
+++ app/src/main/java/space/rodionov/selfanalysis/feature_self_analysis/domain/manager/AnalysisInteractor.kt
@@ -0,0 +1,15 @@
+package space.rodionov.selfanalysis.feature_self_analysis.domain.manager
+
+import space.rodionov.selfanalysis.feature_self_analysis.domain.repository.AnalysisRepo
+import space.rodionov.selfanalysis.feature_self_analysis.domain.repository.PrefRepo
+
+class AnalysisInteractor(
+    private val analysisRepo: AnalysisRepo,
+    private val prefRepo: PrefRepo
+) {
+
+
+
+
+
+}
---
+++ app/src/main/java/space/rodionov/selfanalysis/feature_self_analysis/domain/manager/PrefInteractor.kt
@@ -0,0 +1,13 @@
+package space.rodionov.selfanalysis.feature_self_analysis.domain.manager
+
+import space.rodionov.selfanalysis.feature_self_analysis.domain.repository.AnalysisRepo
+import space.rodionov.selfanalysis.feature_self_analysis.domain.repository.PrefRepo
+
+class PrefInteractor(
+    private val analysisRepo: AnalysisRepo,
+    private val prefRepo: PrefRepo
+) {
+
+
+
+}
