d20331f214dacf1c34fe4447e01b7937d02c0d47
Aleksej <rodionovjr91@gmail.com>
2022-07-07 12:10:16 +0300

    Пишем логику экшнов во вьюмодели,
    далее:
    - рисуем EditAdd экран.
    
    next:
    - make beautiful AnalysisListScreen with search field at the top and navigation to settings

--- app/src/main/java/space/rodionov/selfanalysis/feature_self_analysis/presentation/edit_add_analysis/EditAddAction.kt
+++ app/src/main/java/space/rodionov/selfanalysis/feature_self_analysis/presentation/edit_add_analysis/EditAddAction.kt
@@ -7,6 +7,37 @@ sealed class EditAddAction {
     data class SituationChange(val value: String): EditAddAction()
     data class SituationFocusChange(val focusState: FocusState): EditAddAction()
 
+//    data class EmotionsChange(val value: String): EditAddAction()
+//    data class EmotionsFocusChange(val focusState: FocusState): EditAddAction()
+
+    data class FeelingsChange(val value: String): EditAddAction()
+    data class FeelingsFocusChange(val focusState: FocusState): EditAddAction()
+
+    data class InTheBodyChange(val value: String): EditAddAction()
+    data class InTheBodyFocusChange(val focusState: FocusState): EditAddAction()
+
+    data class WantedToDoChange(val value: String): EditAddAction()
+    data class WantedToDoFocusChange(val focusState: FocusState): EditAddAction()
+
+    data class WhatDoesTheFeelingMeanChange(val value: String): EditAddAction()
+    data class WhatDoesTheFeelingMeanFocusChange(val focusState: FocusState): EditAddAction()
+
+    data class ThoughtsChange(val value: String): EditAddAction()
+    data class ThoughtsFocusChange(val focusState: FocusState): EditAddAction()
+
+    data class FearsChange(val value: String): EditAddAction()
+    data class FearsFocusChange(val focusState: FocusState): EditAddAction()
+
+    data class AskingFromHPChange(val value: String): EditAddAction()
+    data class AskingFromHPFocusChange(val focusState: FocusState): EditAddAction()
+
+    data class InnerCriticChange(val value: String): EditAddAction()
+    data class InnerCriticFocusChange(val focusState: FocusState): EditAddAction()
+
+    data class LovingParentChange(val value: String): EditAddAction()
+    data class LovingParentFocusChange(val focusState: FocusState): EditAddAction()
+
+
 
     object SaveNote: EditAddAction()
 }
